9c1ce3e6cc78dd5ab84d504e3378c4995cfe102f
qinyi <cuiyuhang@aliyun.com>
2022-07-17 16:39:34 +0800

    test 7.17.2.cpp

---
+++ work-timeline/2022.07/test 7.17.2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    size_t size;
+    size_t size2;
+    cout<<size<<endl;
+    cout<<"test"<<endl;
+    cout<<"success"<<endl;
+    system("pause");
+}
+
